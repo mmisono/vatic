9e53aed93e199510a939a48d7b74376d41013c02
John Doherty <doherty1@stanford.edu>
2015-01-28 14:08:34 -0800

    simplified setup

--- vatic-install.sh
+++ vatic-install.sh
@@ -1,11 +1,15 @@
-sudo apt-get install -y git python-setuptools python-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev apache2 libapache2-mod-wsgi mysql-server-5.1 mysql-client-5.1 libmysqlclient-dev gfortran 
+# This is going to be platform dependent
+sudo apt-get install git python-setuptools python-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev apache2 libapache2-mod-wsgi mysql-server-5.5 mysql-client-5.5 libmysqlclient-dev gfortran 
 
-sudo easy_install -U SQLAlchemy wsgilog pil cython mysql-python munkres parsedatetime argparse
-sudo easy_install -U numpy
-
+# Clone revevant repos
 git clone https://github.com/cvondrick/turkic.git
 git clone https://github.com/cvondrick/pyvision.git
-git clone https://github.com/cvondrick/vatic.git
+git clone https://github.com/johndoherty/vatic.git
+
+# Setup virtual environment
+sudo pip install virtualenv
+virtualenv .env
+pip install -r vatic/requirements.txt -U --allow-all-external --allow-unverified PIL
 
 cd turkic
 sudo python setup.py install
@@ -14,6 +18,9 @@ cd ..
 cd pyvision
 sudo python setup.py install
 cd ..
+
+echo "NOTE: If PIL was installed without JPEG, ZLIB, or FREETYPE2 it means it could not find some of the libraries installed earier."
+echo "Follow the instructions at: http://jj.isgeek.net/2011/09/install-pil-with-jpeg-support-on-ubuntu-oneiric-64bits/"
 
 echo "*****************************************************"
 echo "*** Please consult README to finish installation. ***"
