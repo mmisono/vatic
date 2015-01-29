38db5bb3bc1cea2c03aa58f8dce7796e89b029fc
John Doherty <doherty1@stanford.edu>
2015-01-28 23:28:08 -0800

    cleaned up some stuff

--- start_server.py
+++ start_server.py
@@ -2,5 +2,7 @@ import os
 import server
 from config import host, port
 from werkzeug.serving import run_simple
+import logging
 
-run_simple(host, port, server.application, use_reloader=True, static_files={'/':os.path.join(os.path.dirname(__file__), 'public')})
+logging.basicConfig(filename='log.txt', level=logging.INFO)
+run_simple(host, port, server.application, use_debugger=True, use_reloader=True, static_files={'/':os.path.join(os.path.dirname(__file__), 'public')})
