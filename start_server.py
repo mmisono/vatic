daac28c77703d03c99b240a04bdcfe27cc63281c
John Doherty <john.doherty13@gmail.com>
2015-02-04 10:33:43 -0800

    modified server

--- start_server.py
+++ start_server.py
@@ -5,4 +5,4 @@ from werkzeug.serving import run_simple
 import logging
 
 logging.basicConfig(filename='log.txt', level=logging.INFO)
-run_simple(host, port, server.application, use_debugger=True, use_reloader=True, static_files={'/':os.path.join(os.path.dirname(__file__), 'public')})
+run_simple(host, port, server.application, use_reloader=True, static_files={'/':os.path.join(os.path.dirname(__file__), 'public')})
