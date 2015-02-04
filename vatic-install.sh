a5b73fc27193806add9ab982555ea307c7b3b37f
John Doherty <doherty1@stanford.edu>
2015-02-04 11:08:26 -0800

    clarified virtualenv instructions

--- vatic-install.sh
+++ vatic-install.sh
@@ -1,3 +1,8 @@
+# I have found that this works better as part of python virtualenv
+# Before running this create a virtual environment using:
+# $ virtualenv .env
+# $ source .env/bin/activate
+
 # This is going to be platform dependent
 sudo apt-get install git python-setuptools python-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev apache2 libapache2-mod-wsgi mysql-server-5.5 mysql-client-5.5 libmysqlclient-dev gfortran 
 
@@ -9,11 +14,11 @@ git clone https://github.com/johndoherty/vatic.git
 pip install -r vatic/requirements.txt -U --allow-all-external --allow-unverified PIL
 
 cd turkic
-sudo python setup.py install
+python setup.py install
 cd ..
 
 cd pyvision
-sudo python setup.py install
+python setup.py install
 cd ..
 
 echo "NOTE: If PIL was installed without JPEG, ZLIB, or FREETYPE2 it means it could not find some of the libraries installed earier."
